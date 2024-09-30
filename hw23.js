b0d1609b5b29e4ff1726b3a84e7865eb257c2329
lenatnk <99693327+lenatnk@users.noreply.github.com>
2024-09-30 23:17:50 +0200

    updated .js file for hw2.3

--- hw23.js
+++ hw23.js
@@ -1,4 +1,5 @@
-let scores = [90, 85, 78, 92, 66, 100];
+let scores = [90, 85, 78, 92, 66, 100, 49, 76, 88, 95, 84, 74, 91, 62, 45, 59, 98, 82, 67, 72, 56, 53, 87, 68
+];
 
 for (let i = 0; i < scores.length - 1; i++) {
   for (let j = 0; j < scores.length - i - 1; j++) {
